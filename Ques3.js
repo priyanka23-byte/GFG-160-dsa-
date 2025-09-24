293550e0210e860a259bbd11097d35a7da8cbeef
priyanka23-byte <priyankaydv317@gmail.com>
2025-09-24 18:38:01 +0530

    Dsa practice Ques3

---
+++ Ques3.js
@@ -0,0 +1,34 @@
+// Reverse an Array=You are given an array of integers arr[]. You have to reverse the given array.
+
+//bruteforce approach 
+
+function reverseArray(arr){
+    let n= arr.length;
+    let temp =[];
+
+    for (let i=n-1; i>= 0; i-- ){
+        temp.push(arr[i]);
+    }
+    return temp;
+    
+}
+console.log(reverseArray([5,4,7,2,1]));
+
+// tc= (n), sc = 0(n)
+
+//optimised approach = two pointer approach;
+
+function reverseArray(arr){
+    let i =0;
+    let j= arr.length-1;
+
+    while(i<j){
+        [arr[i], arr[j]]= [arr[j],arr[i]];
+        i++;
+        j--;
+
+    }
+    return arr;
+}
+
+
